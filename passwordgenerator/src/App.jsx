0835e431607a00c5b361e17e6873d668933f2eea
Raj Lakshmi <rajlakshmi18704@gmail.com>
2024-03-13 16:58:04 +0530

    passwordLength,checkbox,numbersallowedButton.

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -14,17 +14,70 @@ const passwordGenerator=useCallback(()=>{
 if(number) str+="0123456789"
 if(character)
 str+="!@#$%^&*[]{}~`'"
- for(let i=0;i<=array.length;i++){
-let char=math.floor(Math.random()*str.length+1)
-pass=str.charAt(char)
+ for(let i=1;i<=length;i++){
+let char=Math.floor(Math.random()*str.length+1)
+pass
++=str.charAt(char)
  } setPassword(pass)
 },[number,length,character,setPassword])
 
   return (
-    <>
-     <h1 className='text-6xl  text-center text-white'>Password generator</h1>
-   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-900 bg-gray-800'>ytred</div>
-    </>
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-900 bg-gray-800 h-24'> 
+   <h1 className=' text-center text-white'>Password generator</h1>
+   <div className="flex shadow
+   rounded-lg overflow-hidden mb-4">
+    <input
+    type="text"
+    value={password}
+    className="outline-none w-full py-1 px-3"
+ placeholder="Password"
+    readOnly
+    />
+    <button>copy</button>
+   </div>
+  <div className='flex text-sm gap-x-2'>
+    <div className='flex items-center gap-x-1'>
+     <input type="range"
+     min={6}
+     max={113}
+     value={length}
+     className='cursor-pointer'
+   onChange={(e)=>{
+    {setLength(e.target.value)}
+   }}  />
+     <label>length:{length}</label>
+    </div>
+    <div className='flex items-center gap-x-1'>
+      <input 
+      type="checkbox"
+      defaultChecked={number}
+      id="numberInput"
+      onChange={()=>{
+        setNumber(prev=>
+          !prev);
+      }}
+      />
+      <label>
+        Numbers
+      </label>
+      </div>
+      <div className='flex items-center gap-x-1'>
+      <input 
+      type="checkbox"
+      defaultChecked={number}
+      id="numberInput"
+      onChange={()=>{
+        setNumber(prev=>
+          !prev);
+      }}
+      />
+      <label>
+     Characters
+      </label>
+      </div>  
+  </div>
+ </div>
+
   )
 }
 
